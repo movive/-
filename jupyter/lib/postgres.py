9bd826742bddb1e853d9958d800363ddb6c6fd8c
movive <movive32@hotmail.com>
2019-04-11 22:15:57 +0800

    initial database connection

---
+++ jupyter/lib/postgres.py
@@ -0,0 +1,11 @@
+"""Helper module for interfacing with PostgreSQL."""
+import psycopg2 as pg2
+
+def connect_to_postgres():
+    """Preconfigured to connect to PostgreSQL running on same system. Returns connection and cursor.
+    
+    con, cur = connect_to_postgres()
+    """
+    
+    con = pg2.connect(host='this_postgres', user='postgres', database='postgres')
+    return con, con.cursor()
